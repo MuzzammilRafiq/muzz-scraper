997c9e97209b36d27412ae3f3a2f39c5b4e9bb16
MuzzammilRafiq <mzmlrafiqcse@gmail.com>
2024-11-24 17:34:59 +0530

    Update global styles and layout metadata; refine color variables and improve accessibility

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,8 +2,8 @@ import { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "drawing",
-  description: "drawing",
+  title: "Scaper",
+  description: "Scaper",
 };
 
 export default function RootLayout({

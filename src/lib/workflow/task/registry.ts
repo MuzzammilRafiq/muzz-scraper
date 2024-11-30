15d00b0391e0cf43f0e3f5091224d9f2099a882b
MuzzammilRafiq <mzmlrafiqcse@gmail.com>
2024-11-30 19:27:41 +0530

    Enhance task registry with WorkflowTask type definition and improve type safety

--- src/lib/workflow/task/registry.ts
+++ src/lib/workflow/task/registry.ts
@@ -1,8 +1,14 @@
+import { TaskType } from "~/type/task";
 import { ExtractTextFromElement } from "./ExtractTextFromElement";
 import { LaunchBrowserTask } from "./LaunchBrowser";
 import { PageToHTMLTask } from "./PageToHtml";
+import { WorkflowTask } from "~/type/workflow";
 
-export const TaskRegistry = {
+type Registery = {
+  [K in TaskType]: WorkflowTask & { type: K };
+};
+
+export const TaskRegistry: Registery = {
   LAUNCH_BROWSER: LaunchBrowserTask,
   PAGE_TO_HTML: PageToHTMLTask,
   EXTRACT_TEXT_FROM_ELEMENT: ExtractTextFromElement,
--- src/type/workflow.ts
+++ src/type/workflow.ts
@@ -1,4 +1,15 @@
+import { TaskParam, TaskType } from "./task";
+
 export enum WorkflowStatus {
   DRAFT = "DRAFT",
   PUBLISHED = "PUBLISHED",
 }
+export type WorkflowTask = {
+  label: string;
+  icon: React.ComponentType<{ size: number }>;
+  isEntryPoints: boolean;
+  inputs: TaskParam[];
+  outputs: TaskParam[];
+  type: TaskType;
+  credits: number;
+};
